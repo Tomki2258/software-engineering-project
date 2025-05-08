e25a8687dbd5ac46d509d6077a94319e3a5b1181
tamus <52979692+Tomki2258@users.noreply.github.com>
2025-05-08 12:49:50 +0200

    Create Order.java

---
+++ Restaurant-Manager/src/main/java/org/example/models/Order.java
@@ -0,0 +1,19 @@
+package org.example.models;
+
+import java.util.List;
+
+public class Order {
+    List<Product> productList;
+    public Boolean addProduct(Product product){
+        // TODO warunki sprawdzenia poprawności produktu
+        productList.add(product);
+        return true;
+    }
+    public Boolean removeProduct(int index){
+        if (productList.size() < index) {
+            return false;
+        }
+        productList.remove(index);
+        return true;
+    }
+}
